d406bfbec7e7ccbcfbf938d1fe5434353bf1c8b7
Shaun Crampton <shaun@tigera.io>
2021-09-06 11:39:07 +0100

    Markups: fix typo.

--- pkg/openapi/openapi_generated.go
+++ pkg/openapi/openapi_generated.go
@@ -6190,7 +6190,7 @@ func schema_pkg_apis_projectcalico_v3_IPPoolSpec(ref common.ReferenceCallback) c
 					},
 					"awsSubnetID": {
 						SchemaProps: spec.SchemaProps{
-							Description: "AWSSubnetID if specified Calico will attempt to ensure that IPs chosen from this IP pool are routed to the corresponding node by adding one or more secondary ENIs to the node and explicitly assigning the IP to one of the secondary ENIs.  Important: since subnets cannot cross availability zones, it's important to use Kubernetes node selectors to avoid scheduling pods to one availability zone with IP pool that is backed by a subnet that belongs to another availability zone.",
+							Description: "AWSSubnetID if specified Calico will attempt to ensure that IPs chosen from this IP pool are routed to the corresponding node by adding one or more secondary ENIs to the node and explicitly assigning the IP to one of the secondary ENIs.  Important: since subnets cannot cross availability zones, it's important to use Kubernetes node selectors to avoid scheduling pods to one availability zone using an IP pool that is backed by a subnet that belongs to another availability zone. If AWSSubnetID is specified, then the CIDR of the IP pool must be contained within the specified AWS subnet.",
 							Type:        []string{"string"},
 							Format:      "",
 						},
