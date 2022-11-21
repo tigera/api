21a89e12f6b52b5baf931a1f9e08d83afced9311
Pasan Weerasinghe <pasanweerasinghe@gmail.com>
2022-11-21 13:56:41 -0800

    Sync with v3.15.0

--- metadata.mk
+++ metadata.mk
@@ -6,15 +6,15 @@
 GO_BUILD_VER = v0.76
 
 # Version of Kubernetes to use for tests.
-K8S_VERSION     = v1.24.3
-# This is used for lachlanevenson/k8s-kubectl and kubectl binary release.
-KUBECTL_VERSION = v1.24.3
+K8S_VERSION     = v1.24.7
+# This is used for bitnami/kubectl and kubectl binary release in compliance benchmarker, confd, and kube-controllers.
+KUBECTL_VERSION = v1.24.8
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
 ETCD_VERSION=v3.5.1
 PROTOC_VER=v0.1
-UBI_VERSION=8.6
+UBI_VERSION=8.7
 
 # Configuration for Semaphore integration.
 ORGANIZATION = tigera
