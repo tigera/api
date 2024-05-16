f7b4c11f8d45f3796e8010adda3feb4cb267df13
Daniel Fox <dan.fox@tigera.io>
2024-05-16 10:36:09 -0700

    Automatic API update from tigera/calico-private v3.20.0-1.0-calient-0.dev-495-g97a31636fe

--- metadata.mk
+++ metadata.mk
@@ -3,21 +3,22 @@
 #################################################################################################
 
 # The version of github.com/projectcalico/go-build to use.
-GO_BUILD_VER=v0.90
+GO_BUILD_VER=v0.91
 # Env var to ACK Ginkgo deprecation warnings, may need updating with go-build.
 ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
 
 # Version of Kubernetes to use for tests, bitnami/kubectl, and kubectl binary release in
 # compliance benchmarker, confd, and kube-controllers.
-K8S_VERSION=v1.27.11
+K8S_VERSION=v1.28.9
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
 ELASTIC_VERSION=7.17.18
 ETCD_VERSION=v3.5.6
 HELM_VERSION=v3.11.3
-KINDEST_NODE_VERSION=v1.24.7
-KIND_VERSION=v0.14.0
+KINDEST_NODE_VERSION=v1.27.11
+KINDEST_NODE_VERSION_DUAL_TOR=v1.24.7
+KIND_VERSION=v0.22.0
 PROTOC_VER=v0.1
 UBI8_VERSION=8.9
 UBI9_VERSION=9.3
@@ -63,3 +64,15 @@ WINDOWS_DIST = dist/windows
 WINDOWS_HPC_VERSION ?= v1.0.0
 # The Windows versions used as base for Calico Windows images
 WINDOWS_VERSIONS ?= 1809 ltsc2022
+
+# THIRD_PARTY_REGISTRY configures the third-party registry that serves intermediate base image
+# for some Calico Enterprise components. They are never released directly to public.
+ifeq ($(SEMAPHORE_GIT_REF_TYPE), branch)
+    # on master and release-calient branches
+    THIRD_PARTY_REGISTRY=gcr.io/unique-caldron-775/cnx/tigera/third-party
+else ifeq ($(SEMAPHORE_GIT_REF_TYPE), pull-request)
+    # on pull requests
+    THIRD_PARTY_REGISTRY=gcr.io/unique-caldron-775/third-party-ci
+else
+    THIRD_PARTY_REGISTRY=gcr.io/tigera-dev/third-party-ci
+endif
