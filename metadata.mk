b9b97fc1e412a8e845985bd0d01e9589fca09c70
Daniel Fox <dan.fox@tigera.io>
2025-05-16 07:27:58 -0700

    API update for v3.19.0-2.0

--- metadata.mk
+++ metadata.mk
@@ -9,19 +9,19 @@ ACK_GINKGO=ACK_GINKGO_DEPRECATIONS=1.16.5
 
 # Version of Kubernetes to use for tests, bitnami/kubectl, and kubectl binary release in
 # compliance benchmarker, confd, and kube-controllers.
-K8S_VERSION=v1.28.11
+K8S_VERSION=v1.28.9
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
-ELASTIC_VERSION=7.17.21
+ELASTIC_VERSION=7.17.18
 ETCD_VERSION=v3.5.6
 HELM_VERSION=v3.11.3
 KINDEST_NODE_VERSION=v1.27.11
 KINDEST_NODE_VERSION_DUAL_TOR=v1.24.7
 KIND_VERSION=v0.22.0
 PROTOC_VER=v0.1
-UBI8_VERSION=8.10
-UBI9_VERSION=9.4
+UBI8_VERSION=8.9
+UBI9_VERSION=9.3
 
 # Configuration for Semaphore integration.
 ORGANIZATION=tigera
