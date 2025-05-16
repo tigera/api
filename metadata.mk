9dbc3ef20ddc9123ebe35cae0da2d751a3446fc4
Daniel Fox <dan.fox@tigera.io>
2025-05-16 07:49:38 -0700

    API update for v3.18.5

--- metadata.mk
+++ metadata.mk
@@ -7,17 +7,17 @@ GO_BUILD_VER=v0.89
 
 # Version of Kubernetes to use for tests, bitnami/kubectl, and kubectl binary release in
 # compliance benchmarker, confd, and kube-controllers.
-K8S_VERSION=v1.27.14
+K8S_VERSION=v1.28.12
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
-ELASTIC_VERSION=7.17.21
+ELASTIC_VERSION=7.17.22
 ETCD_VERSION=v3.5.6
 # FIXME upgrading to kindest/node newer than v1.24.7 causes Node/kind-cluster and sig-network conformance
 # tests to timeout or fail.
 KINDEST_NODE_VERSION=v1.24.7
 PROTOC_VER=v0.1
-UBI_VERSION=8.9
+UBI_VERSION=8.10
 
 # Configuration for Semaphore integration.
 ORGANIZATION=tigera
