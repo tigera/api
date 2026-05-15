8f9049d66d4ed3fd1ce79dccc60d2d864f7b44d9
caseydavenport <1944093+caseydavenport@users.noreply.github.com>
2026-05-15 16:42:40 +0000

    Automatic API update from tigera/calico-private release-calient-v3.23

--- metadata.mk
+++ metadata.mk
@@ -1,17 +1,20 @@
 #################################################################################################
 # This file contains Makefile configuration parameters and metadata for this branch.
 #################################################################################################
+# The project Go version
+GO_VERSION=1.25.10
+# Version of Kubernetes to use for dependencies, tests, registry.k8s.io/kubectl, and kubectl binary release.
+K8S_VERSION=v1.35.4
+# The version of LLVM to use for go-build and calico/base images.
+LLVM_VERSION=18.1.8
 
 # Calico toolchain versions and the calico/base image to use.
-GO_BUILD_VER=1.25.10-llvm18.1.8-k8s1.35.4
+GO_BUILD_VER=$(GO_VERSION)-llvm$(LLVM_VERSION)-k8s$(K8S_VERSION:v%=%)
 RUST_BUILD_VER=1.93.1
 
 # Calico Enterprise shipping images now builds on UBI 10. For Calico OSS to Enterprise merges,
 # please don't downgrade the base image back to UBI 9.
 CALICO_BASE_VER=ubi10-1778780765
-
-# Version of Kubernetes to use for tests, rancher/kubectl, and kubectl binary release.
-K8S_VERSION=v1.35.2
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
