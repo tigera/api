417be403e77b53d8a4e14de1c18bfaef65509cd0
caseydavenport <1944093+caseydavenport@users.noreply.github.com>
2026-05-28 22:38:05 +0000

    Automatic API update from tigera/calico-private release-calient-v3.23

--- metadata.mk
+++ metadata.mk
@@ -14,7 +14,7 @@ RUST_BUILD_VER=1.93.1
 
 # Calico Enterprise shipping images now builds on UBI 10. For Calico OSS to Enterprise merges,
 # please don't downgrade the base image back to UBI 9.
-CALICO_BASE_VER=ubi10-1778780765
+CALICO_BASE_VER=ubi10-1779921302
 
 # Version of various tools used in the build and tests.
 COREDNS_VERSION=1.5.2
